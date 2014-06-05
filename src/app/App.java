f1810be00035b5aeddf7a706e6db7de974de6286
Riley Shaw <rlyshw@gmail.com>
2014-06-05 17:29:31 -0400

    Board creates checkered pattern

--- src/app/App.java
+++ src/app/App.java
@@ -5,7 +5,6 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Board board = new Board(8);
-		System.out.println("x:"+board.tileList.get(0).x+" y:"+board.tileList.get(1).y); //Hey it works
 	}
 
 }
--- src/app/Board.java
+++ src/app/Board.java
@@ -6,10 +6,26 @@ import java.util.List;
 public class Board {
 	public List<Tile> tileList = new ArrayList<Tile>(); //Creates a List object consisting of Tile objects
 	public Board(int size) {
-		if(size==8 || size==10 || size==12){ //These are the only sizes of checker boards
-			for(int x=0;x<size;x++){
-				for(int y=0;y<size;y++) {
-					tileList.add(new Tile(false, false, x, y)); //For every row and column, add a Tile
+		int startRows = (size-2)/2;
+		if(size==8 || size==10 || size==12){ // These are the only sizes of checker boards
+			for(int x=1;x<=size;x++){
+				for(int y=1;y<=size;y++) { // For every row and column, add a Tile
+					// TODO Handle the placement of virtual checkers using purely math. 
+					// If (x is even) XOR (y is even), then state is 1 or 2, depending on the current row
+					if((x%2==0)^(y%2==0)) {
+						if(x<=startRows){ //Starting positions
+							tileList.add(new Tile(2, x, y)); 
+						}
+						else if(x>startRows+2) {
+							tileList.add(new Tile(1, x, y));
+						}
+						else {
+							tileList.add(new Tile(0, x, y));
+						}
+					}
+					else {
+						tileList.add(new Tile(0,x,y));
+					}
 				}
 			}
 		}
