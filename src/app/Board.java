c4341b9263ef93c774b38bad96e732bb51ba9f02
Riley Shaw <rlyshw@gmail.com>
2014-06-05 16:57:49 -0400

    comments

--- src/app/Board.java
+++ src/app/Board.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-	public List<Tile> tileList = new ArrayList<Tile>();
+	public List<Tile> tileList = new ArrayList<Tile>(); //Creates a List object consisting of Tile objects
 	public Board(int size) {
-		if(size==8 || size==10 || size==12){
+		if(size==8 || size==10 || size==12){ //These are the only sizes of checker boards
 			for(int x=0;x<size;x++){
 				for(int y=0;y<size;y++) {
-					tileList.add(new Tile(false, false, x, y));
+					tileList.add(new Tile(false, false, x, y)); //For every row and column, add a Tile
 				}
 			}
 		}
 		else{
-			System.out.println("Error, board must be 8, 10, or 12 tiles in size");
+			System.out.println("Error, board must be 8, 10, or 12 tiles in size"); //Handle board size 
 		}
 	}
 }
