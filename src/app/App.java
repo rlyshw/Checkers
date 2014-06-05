7fd606f863984f5733747ee0a4db7d1776826edb
irgraham <irgraham1@gmail.com>
2014-06-05 14:05:25 -0400

    +Tile class

--- src/app/App.java
+++ src/app/App.java
@@ -5,6 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Different Code!");
+		Tile seven = new Tile(true,false,1,5);
+		System.out.println(seven.isPlayer1);
 	}
 
 }
