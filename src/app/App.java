d34e45441f5821ac98ae5996514b465a06696897
GrahamLaptop <GrahamLaptop@192.168.1.116>
2014-06-09 18:11:02 -0400

    Yeah, done for the time being.

--- src/app/App.java
+++ src/app/App.java
@@ -6,11 +6,14 @@ public class App {
 		// TODO Auto-generated method stub
 		Board board = new Board(8);
 		// TODO Form the logic within a do-while clause
+		boolean isPlayer1 = true;
+		
 		do {
 			do {
-				
+				// Logic related to the mouse clicks.
 			}
-			while (!true);
+			while (!board.validateMove()); // Probably will end up being some method of the mouse-click handler.  board.validateMove() is a place holder boolean.
+			isPlayer1 = !isPlayer1;
 		}
 		while (!board.checkEndGame());
 	}
